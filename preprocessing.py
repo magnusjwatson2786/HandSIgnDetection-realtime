ec1819ed1f54a3dfc49426f42a31e9b4f210b702
magnusjwatson2786 <mearunprabhakar14@gmail.com>
2024-03-10 21:34:59 +0530

    add bulk preprocessing with progressbar

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,6 @@ import numpy as np
 # Load MediaPipe Hands model
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1)
-failed = []
 
 def preprocess(image_path, fails):
     image_name = image_path.split('/')[-1] # filename
@@ -48,13 +47,37 @@ def preprocess(image_path, fails):
             # blank_slate = cv2.threshold(blank_slate, 1, 255, cv2.THRESH_BINARY)[1] # not really needed
             # cv2.imshow("1", blank_slate)
             cv2.imwrite(dest_path, blank_slate)
-            print(dest_path)
+            # print(dest_path)
     else:
         fails.append(image_path)
 
-path = 'hagrid_dataset_512/four/'
-for i in os.listdir(path):
-    preprocess(path+i, failed)
+def printProgressBar (iteration, total, prefix = '', suffix = '', length = 100, fill = '█', printEnd = "\r"):
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {iteration} {suffix}', end = printEnd)
+    if iteration == total: 
+        print()
+
+done = os.listdir('preprocessed_data/')
+total = os.listdir('hagrid_dataset_512/')
+not_done = [ i for i in total if i not in done]
+print(not_done)
+parent_path = 'hagrid_dataset_512/'
+for i in not_done:
+    path = parent_path + i +'/'
+    files = os.listdir(path)
+    count = 0
+    total = len(files)
+    failed = []
+    printProgressBar(count, total, i, 'image(s) sucessfully converted', 60)
+    for j in files:
+        preprocess(path+j, failed)
+        count += 1
+        printProgressBar(count, total, i, 'image(s) sucessfully converted', 60)
+    print(f'{len(failed)} images couldnt be converted')
+    with open('failed/'+i+'-failed.txt', 'w') as f:
+        for item in failed:
+            f.write(item + '\n')
 
 
 cv2.waitKey(0)
